4a5f2e651968cbe0338a11598a7348bdce9acef1
Yeaseen <yeaseen.arafat96@gmail.com>
2018-07-09 22:02:57 +0600

    Nearest negh and nearest insertion done

--- src/TSP.java
+++ src/TSP.java
@@ -1,32 +1,41 @@
-import org.omg.PortableInterceptor.INACTIVE;
 
 import java.util.ArrayList;
 
 class data{
-   int x, y;
+   int x, y,idN;
    boolean visited;
 
-   public data(int xx, int yy){
+   public data(int xx, int yy,int id){
        x=xx;
        y=yy;
+       idN=id;
        visited=false;
    }
+   public data(int id){
+       idN=id;
+       System.out.println("idN==========="+idN);
+   }
 }
+
+
 
 public class TSP {
 
     static double[][] adjMat=null;
     static ArrayList<data> nodeCO= new ArrayList<data>();
-    static String path=null;
+    static ArrayList<data> paTH= new ArrayList<data>();
+    static double cost=0;
+
 
     public static void main(String[] args) {
 
         int n= Integer.parseInt(args[0]);
         System.out.println("Number of Nodes: " + n);
         for (int i = 0; i < n ; i++) {
-            nodeCO.add(new data(Integer.parseInt(args[i*2+1]),Integer.parseInt(args[i*2+2])));
+            nodeCO.add(new data(Integer.parseInt(args[i*2+1]),Integer.parseInt(args[i*2+2]),i));
             System.out.println(nodeCO.get(i).x+"  "+nodeCO.get(i).y);
         }
+
         adjMat=new double[n][n];
 
         for (int i = 0; i < n ; i++) {
@@ -50,18 +59,86 @@ public class TSP {
             System.out.println();
         }
         System.out.println();
-        path="5-->";
-        //nodeCO.get(0).visited=true;
-        getTSPPath(5);
-        path+="5";
-        System.out.println();
-        System.out.println(path);
 
 
 
-        //System.out.println(getMinimumOfColumn(3));
+
+
+
+        //func1(2);
+        func2(10);
+        System.out.print("PATH IS: ");
+        for (int i = 0; i < paTH.size() ; i++) {
+            if(i<paTH.size()-1)System.out.print(paTH.get(i).idN+"-->");
+            else System.out.print(paTH.get(i).idN);
+
+            if(i<=paTH.size()-2) {
+                cost+=adjMat[paTH.get(i).idN][paTH.get(i+1).idN];
+            }
+        }
+        System.out.println("\nCOST: "+cost);
 
     }
+
+
+    public static void func2(int i){
+        nodeCO.get(i).visited=true;
+        paTH.add(0,nodeCO.get(i));
+
+        int dest1= getMinimumOfColumn(i);
+        nodeCO.get(dest1).visited=true;
+        paTH.add(1,nodeCO.get(dest1));
+        paTH.add(2,nodeCO.get(i));
+
+        while (checkNonVisited())
+        {
+            int minNode=0;
+            for (int j = 0; j < paTH.size()-1 ; j++) {
+                //System.out.println("idN"+paTH.get(j).idN);
+                minNode=getMinimumOfColumn(paTH.get(j).idN);
+            }
+            System.out.println("min Node--> "+minNode);
+
+            int destM=0;
+            double min= Double.MAX_VALUE;
+            System.out.println("path size=="+paTH.size());
+            for (int j = 0; j < paTH.size()-1 ; j++) {
+                //System.out.println("j:  "+j);
+                //System.out.println(adjMat[paTH.get(j).idN][minNode]+ " "+adjMat[minNode][paTH.get(j+1).idN]+ " "+adjMat[paTH.get(j).idN]
+                    //    [paTH.get(j+1).idN]);
+                double x=adjMat[paTH.get(j).idN][minNode]+adjMat[minNode][paTH.get(j+1).idN]-adjMat[paTH.get(j).idN]
+                        [paTH.get(j+1).idN];
+                //System.out.println("x: "+x);
+                if(x<min){
+                    min=x;
+                    destM=j;
+                }
+
+            }
+            nodeCO.get(minNode).visited=true;
+            paTH.add(destM+1,nodeCO.get(minNode));
+            //System.out.println("put in "+destM);
+
+        }
+
+
+
+
+
+
+
+    }
+
+    public static void func1(int i){
+        paTH.add(nodeCO.get(i));
+        getTSPPath(i);
+        paTH.add(nodeCO.get(i));
+
+
+    }
+
+
+
 
     public static double getEucDIs(int p,int q, int r, int s){
         double m= Math.abs(p - r);
@@ -81,7 +158,8 @@ public class TSP {
         nodeCO.get(sNode).visited=true;
         while (checkNonVisited()){
             int explored=getMinimumOfColumn(sNode);
-            path+=explored+"-->";
+            paTH.add(nodeCO.get(explored));
+
             getTSPPath(explored);
         }
     }
@@ -96,7 +174,7 @@ public class TSP {
                     res=j;
                 }
             }
-        System.out.println( "Minimum of column " + col + " = " + min );
+        //System.out.println( "Minimum of column " + col + " = " + min );
             return res;
 
 
